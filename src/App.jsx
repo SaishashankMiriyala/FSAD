78292ce752b5f2625d47757c36a8656eee4d499d
SaishashankMiriyala <saishashank.miriyala@gmail.com>
2026-04-07 15:00:24 +0530

    exercise12

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,10 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Dashboard from "./components/Dashboard";
-import LocalUserList from "./components/LocalUserList";
-import UserList from "./components/UserList";
-import FakePostList from "./components/FakePostList";
-import "./App.css";
-function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/local-users" element={<LocalUserList />} />
-        <Route path="/users-api" element={<UserList />} />
-        <Route path="/fake-posts" element={<FakePostList />} />
-      </Routes>
-    </Router>
-  );
-}
+import StudentList from "./components/StudentList"; 
+function App() { 
+return ( 
+<div> 
+<h1>Student Management System</h1> 
+<StudentList /> 
+</div> 
+); 
+} 
 export default App;
